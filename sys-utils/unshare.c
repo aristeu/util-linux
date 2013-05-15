d8896b87b414623f22ff97f34613cb0a6580cb06
Aristeu Rozanski <arozansk@redhat.com>
2013-05-15 15:11:36 -0400

    unshare: add options to control UID and GID map
    One of the steps while creating a new user namespace is to setup UID and
    GID maps. In order to do that, a new namespace should be created then
    another process writes /proc/<pid>/{uid,gid}_map.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- sys-utils/unshare.c
+++ sys-utils/unshare.c
@@ -24,6 +24,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "nls.h"
 #include "c.h"
@@ -46,6 +48,16 @@ static void usage(int status)
 	fputs(_(" -n, --net         unshare network namespace\n"), out);
 	fputs(_(" -p, --pid         unshare pid namespace\n"), out);
 	fputs(_(" -U, --user        unshare user namespace\n"), out);
+	fputs(_(" -M, --usermap <a:b:c,...>\n"), out);
+	fputs(_("                   specify the user namespace UID map\n"), out);
+	fputs(_("                   'a' is the starting UID on the current namespace\n"), out);
+	fputs(_("                   'b' is the starting UID on the new namespace\n"), out);
+	fputs(_("                   'c' is the length of this block\n"), out);
+	fputs(_(" -N, --groupmap <a:b:c,...>\n"), out);
+	fputs(_("                   specify the user namespace GID map\n"), out);
+	fputs(_("                   'a' is the starting GID on the current namespace\n"), out);
+	fputs(_("                   'b' is the starting GID on the new namespace\n"), out);
+	fputs(_("                   'c' is the length of this block\n"), out);
 
 	fputs(USAGE_SEPARATOR, out);
 	fputs(USAGE_HELP, out);
@@ -53,6 +65,85 @@ static void usage(int status)
 	fprintf(out, USAGE_MAN_TAIL("unshare(1)"));
 
 	exit(status);
+}
+
+static int write_idmap(char *type, char *map, long pid)
+{
+	FILE *file;
+	unsigned int a, b, c;
+	char path[PATH_MAX], *ptr, *saved = NULL;
+
+	snprintf(path, PATH_MAX, "/proc/%ld/%s_map", pid, type);
+	file = fopen(path, "w");
+	if (file == NULL)
+		return 1;
+
+	for (ptr = strtok_r(map, ",", &saved); ptr;
+	     ptr = strtok_r(map, ",", &saved)) {
+		if (sscanf(ptr, "%u:%u:%u", &a, &b, &c) != 3)
+			return 1;
+		if (fprintf(file, "%u %u %u\n", a, b, c) <= 0)
+			return 1;
+	}
+	fclose(file);
+	return 0;
+}
+
+static void prepare_userns(char *usermap, char *groupmap, int *pipe_fd)
+{
+	pid_t pid;
+	int rc, i;
+	char c;
+
+	if (pipe(pipe_fd) == -1)
+		err(EXIT_FAILURE, _("pipe creation failed"));
+
+	pid = fork();
+	if (pid < 0)
+		err(EXIT_FAILURE, _("fork failed"));
+
+	if (!pid) {
+		close(pipe_fd[1]);
+
+		/* wait for the parent to write the rules */
+		rc = read(pipe_fd[0], &c, 1);
+		if (rc < 0)
+			err(EXIT_FAILURE, _("pipe read error"));
+		else if (rc > 0)
+			errx(EXIT_FAILURE, _("aborted"));
+
+		return;
+	}
+
+	for (i = 0; i < 3; i++) {
+		if (tcsetpgrp(i, pid)) {
+			write(pipe_fd[0], "a", 1);
+			err(EXIT_FAILURE, _("tcsetpgrp"));
+		}
+	}
+
+	/* will wait for the child to be ready */
+	rc = read(pipe_fd[1], &c, 1);
+	if (rc < 0)
+		err(EXIT_FAILURE, _("pipe read error"));
+	else if (rc > 0)
+		errx(EXIT_FAILURE, _("pipe read should be empty"));
+
+	if (usermap && write_idmap("uid", usermap, pid)) {
+		write(pipe_fd[0], "a", 1);
+		err(EXIT_FAILURE, _("failed to write usermap"));
+	}
+
+	if (groupmap && write_idmap("gid", groupmap, pid)) {
+		write(pipe_fd[0], "a", 1);
+		err(EXIT_FAILURE, _("failed to write groupmap"));
+	}
+
+	close(pipe_fd[0]);
+
+	if (wait(&rc) == -1)
+		err(EXIT_FAILURE, _("waiting for process to end"));
+	exit(rc);
 }
 
 int main(int argc, char *argv[])
@@ -66,6 +157,8 @@ int main(int argc, char *argv[])
 		{ "net", no_argument, 0, 'n' },
 		{ "pid", no_argument, 0, 'p' },
 		{ "user", no_argument, 0, 'U' },
+		{ "usermap", 1, 0, 'M' },
+		{ "groupmap", 1, 0, 'M' },
 		{ NULL, 0, 0, 0 }
 	};
 
@@ -73,12 +166,18 @@ int main(int argc, char *argv[])
 
 	int c;
 
+	int pipe_fd[2];
+
+	char *usermap = NULL;
+
+	char *groupmap = NULL;
+
 	setlocale(LC_MESSAGES, "");
 	bindtextdomain(PACKAGE, LOCALEDIR);
 	textdomain(PACKAGE);
 	atexit(close_stdout);
 
-	while ((c = getopt_long(argc, argv, "hVmuinpU", longopts, NULL)) != -1) {
+	while ((c = getopt_long(argc, argv, "hVmuinpUM:N:", longopts, NULL)) != -1) {
 		switch (c) {
 		case 'h':
 			usage(EXIT_SUCCESS);
@@ -103,6 +202,12 @@ int main(int argc, char *argv[])
 		case 'U':
 			unshare_flags |= CLONE_NEWUSER;
 			break;
+		case 'M':
+			usermap = strdup(optarg);
+			break;
+		case 'N':
+			groupmap = strdup(optarg);
+			break;
 		default:
 			usage(EXIT_FAILURE);
 		}
@@ -110,6 +215,9 @@ int main(int argc, char *argv[])
 
 	if (-1 == unshare(unshare_flags))
 		err(EXIT_FAILURE, _("unshare failed"));
+
+	if ((unshare_flags & CLONE_NEWUSER) && (usermap || groupmap))
+		prepare_userns(usermap, groupmap, pipe_fd);
 
 	if (optind < argc) {
 		execvp(argv[optind], argv + optind);
